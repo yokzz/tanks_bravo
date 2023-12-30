be244a36c7f3ac31868853863df765804e165892
yokzz <67423554+yokzz@users.noreply.github.com>
2023-12-30 13:32:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,45 @@
 import pygame
 from settings import settings
-from tanks.player import Player
-from map.level import Level
+from tanks.tank import *
+from tanks.player import *
+from tanks.bullet import *
+from map.level import *
+from tanks.explosion import *
 
+FPS = 30
 pygame.init()
 
 screen = pygame.display.set_mode((settings.screen.width, settings.screen.height))
-pygame.display.set_caption("Bravo Tanks")
-icon = pygame.image.load('Visual/images/icon.png').convert_alpha()
-pygame.display.set_icon(icon)
+clock = pygame.time.Clock()
 
+pygame.display.set_caption("Bravo Tanks")
+icon = pygame.image.load('visual/images/icon.png').convert_alpha()
+pygame.display.set_icon(icon)
+screen.fill([0, 0, 0])
+level1 = Level()
+player1 = Player(level=level1)
 running = True
 while running:
-    # player1 = Player()
-    # player1.draw()
-    # player1.move()
-    level1 = Level()
-    level1.draw()
-    
-    pygame.display.update()
-    for event in pygame.event.get(): 
-        if event.type == pygame.QUIT: # exit
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player1.fire()
+
+        if event.type == pygame.QUIT:  # exit
             running = False
-            
+
+    player1.move()
+    for bullet in player1.bullets:
+        bullet.update()
+
+    screen.fill((0, 0, 0))  # Очистить экран
+    level1.draw(screen)
+    player1.draw()
+    for bullet in player1.bullets:
+        bullet.draw()
+
+    pygame.display.flip()
+    clock.tick(FPS)
+
+pygame.quit()
     
